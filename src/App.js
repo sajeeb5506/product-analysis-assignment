dcb255f283a3ae064028bb0cd85544c86758b56e
sajeeb5506 <sajeebsarker5506@gmail.com>
2022-04-03 23:34:37 +0600

    Components added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import Dashboard from './Components/Dashboard/Dashboard';
 import Heder from './Components/Header/Heder';
 import Home from './Components/Home/Home';
+import Reviews from './Components/Reviews/Reviews';
 
 function App() {
   return (
     <div>
       <Heder/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home></Home>}/>
+        <Route path='reviews' element={<Reviews/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='blogs' element={<Blogs/>}/>
+        <Route path='dashboard' element={<Dashboard/>}/>
+
       </Routes>
     </div>
   );
--- src/Components/Header/Heder.js
+++ src/Components/Header/Heder.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 import './Header.css'
 const Heder = () => {
     return (
         <div>
-            <h1>this is Header..</h1>
+        <nav>
+           <Link to='/'>Home</Link>
+           <Link to="reviews">Reviews</Link>
+           <Link to="dashboard">Dashboard</Link>
+           <Link to="blogs">Blogs</Link>
+           <Link to="about">About</Link>
+        </nav>
         </div>
     );
 };
